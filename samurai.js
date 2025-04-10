5f5119b8ea404d79df75162199301c17189945bb
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-04-10 10:53:24 -0400

    Update samurai.js

--- samurai.js
+++ samurai.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 800;
-canvas.height = 600;
+canvas.width = 100%;
+canvas.height = 100%;
 
 class Player {
     constructor() {
