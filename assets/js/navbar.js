57575144f58b87366c99331ec5928a69903a3b7c
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-09-18 13:25:12 -0400

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,17 +1,18 @@
 async function loadDynamicContent(targetElementId, sourcePageUrl) {
-    try {
-        const response = await fetch(sourcePageUrl);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const htmlContent = await response.text();
-        document.getElementById(targetElementId).insertAdjacentHTML('beforeend', htmlContent);
-    } catch (error) {
-        console.error('Error loading dynamic content:', error);
+  try {
+    const response = await fetch(sourcePageUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+    const htmlContent = await response.text();
+    document.getElementById(targetElementId).insertAdjacentHTML('beforeend', htmlContent);
+  } catch (error) {
+    console.error('Error loading dynamic content:', error);
+  }
 }
 
-// Call the function to load content when the page loads or based on an event
+// Call the function to load content when the page loads
 document.addEventListener('DOMContentLoaded', () => {
-    loadDynamicContent('nav-container', './navbar.html');
+  // Load the navbar.html into the element with id="nav-container"
+  loadDynamicContent('nav-container', './navbar.html');
 });
