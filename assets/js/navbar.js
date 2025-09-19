c5e11061e73911e3679955ad4ec957937a310743
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-09-19 13:10:41 -0400

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,8 +1,17 @@
-fetch('./navbar.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#nav-container");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
+var newScript = document.createElement('script');
+newScript.type = 'text/javascript';
+newScript.src = 'http://code.jquery.com/jquery-latest.js'; // set the source of the script to your script
+newScript.onload = function() {
+  console.log("Script is ready!");
+  $(document).ready(function() {
+    console.log("JQuery is ready!");
+  });
+};
+var head = document.getElementsByTagName("head")[0];
+head.appendChild(newScript);
+
+$( document ).ready(function() {
+    $.get("navbar.html", function(data){
+        $("#nav-container").replaceWith(data);
+    });
+});
