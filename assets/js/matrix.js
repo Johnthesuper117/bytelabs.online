df7185b88fce14ea72c35b67c47ccf07dda21740
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-09-29 22:26:24 +0000

    better matrix efx?

--- assets/js/matrix.js
+++ assets/js/matrix.js
@@ -21,6 +21,8 @@ for( let x = 0; x < columns; x++ ) {
 }
 
 const draw = () => {
+    context.fillStyle = '#0F0';
+    context.font = fontSize + 'px monospace';
 
     context.fillStyle = 'rgba(0, 0, 0, 0.05)';
     context.fillRect(0, 0, canvas.width, canvas.height);
