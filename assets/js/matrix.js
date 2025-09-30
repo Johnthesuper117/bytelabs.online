9751647b50c92e291793068a15b2939e1c2dce33
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-09-30 13:54:52 +0000

    Fixed matrix effect

--- assets/js/matrix.js
+++ assets/js/matrix.js
@@ -21,13 +21,12 @@ for( let x = 0; x < columns; x++ ) {
 }
 
 const draw = () => {
-    context.fillStyle = '#0F0';
-    context.font = fontSize + 'px monospace';
 
     context.fillStyle = 'rgba(0, 0, 0, 0.05)';
     context.fillRect(0, 0, canvas.width, canvas.height);
     context.fillStyle = '#0F0';
     context.font = fontSize + 'px monospace';
+    
     for(let i = 0; i < rainDrops.length; i++)
     {
         const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
