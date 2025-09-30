99925531cbae0824970c98f62695f15a880b9195
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-09-30 15:28:59 +0000

    connecting data

--- assets/js/serverData.js
+++ assets/js/serverData.js
@@ -1,5 +1,5 @@
 import { neon } from '@netlify/neon';
-const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
+const sql = neon(process.env.DATABASE_URL);
 const [post] = await sql`SELECT * FROM posts WHERE id = ${postId}`;
 
 // Example Node.js Netlify Function
@@ -26,3 +26,19 @@ exports.handler = async (event, context) => {
     };
     }
 };
+
+require("dotenv").config();
+
+const http = require("http");
+const { neon } = require("@neondatabase/serverless");
+
+const requestHandler = async (req, res) => {
+  const result = await sql`SELECT version()`;
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
+};
+
+http.createServer(requestHandler).listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
