2224d7b9019c7b172950d6c89dd8363aed9992d7
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-12-07 19:52:54 +0000

    It's ready!

--- assets/js/linkLoader.js
+++ assets/js/linkLoader.js
@@ -4,7 +4,7 @@ function checkChance(intendedUrl) {
     
     var randomNumber = Math.floor(Math.random() * 100) + 1;
 
-    if (randomNumber === 0) { //for now it's 0% chance, but will change to 1% when ready
+    if (randomNumber === 1) { //for now it's 0% chance, but will change to 1% when ready
         location.href = rareUrl; 
     } else {
         location.href = intendedUrl; 
